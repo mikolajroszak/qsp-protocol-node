1fd5309e3721e79522e6368ef034a83cd5138e66
Alex Murashkin <1721409+alex-murashkin@users.noreply.github.com>
2018-03-15 02:03:44 -0400

    Gas price set explicitly to 0

--- qsp_network_audit/utils/eth/tx.py
+++ qsp_network_audit/utils/eth/tx.py
@@ -2,8 +2,8 @@ def mk_args(config):
     gas = config.default_gas
 
     if gas is None:
-        args = {'from': config.account}
+        args = {'from': config.account, 'gasPrice': 0}
     else:
-        args = {'from': config.account, 'gas': int(gas)}
+        args = {'from': config.account, 'gas': int(gas), 'gasPrice': 0}
     
     return args
