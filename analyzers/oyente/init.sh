80ea22ea2e97833461231a5bf8d8d4f1e9eab969
alex-murashkin <alexmur90@gmail.com>
2018-05-31 17:49:56 -0400

    Analyzer repo URL updated

--- analyzers/oyente/init.sh
+++ analyzers/oyente/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 $(aws ecr get-login --no-include-email --region us-east-1) # this won't be necessary once we switch to public images
-docker pull 466368306539.dkr.ecr.us-east-1.amazonaws.com/qsp-analyzer-oyente:dev
+docker pull 466368306539.dkr.ecr.us-east-1.amazonaws.com/melonproject-oyente:57dcfae35773ff30aa34856865a7ad07c501d4d0
--- analyzers/oyente/run.sh
+++ analyzers/oyente/run.sh
@@ -3,5 +3,5 @@ cp "$1" /tmp/
 # On Mac, by default, only a few locations can be used for mounting
 # Copy the file to /tmp/ and then extract the output from there
 basename=$(basename "$1")
-docker run -v /tmp/:/shared/ -i 466368306539.dkr.ecr.us-east-1.amazonaws.com/qsp-analyzer-oyente:dev bash -c "python /oyente/oyente/oyente.py -j -s /shared/$basename"
+docker run -v /tmp/:/shared/ -i 466368306539.dkr.ecr.us-east-1.amazonaws.com/melonproject-oyente:57dcfae35773ff30aa34856865a7ad07c501d4d0 bash -c "python /oyente/oyente/oyente.py -j -s /shared/$basename"
 cp "/tmp/$basename.json" "$1.json"
