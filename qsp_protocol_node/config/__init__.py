338ca492f21fda50d616a510681ed5154f763e3c
Martin Derka <martin.derka@seznam.cz>
2018-07-05 21:01:37 +0200

    QSP-148 Moved logging initialization to init. (#74)

--- qsp_protocol_node/config/__init__.py
+++ qsp_protocol_node/config/__init__.py
@@ -1,10 +1,60 @@
+import logging
+import logging.config
+
 from .config import config_value, Config
 from .config_utils import ConfigUtils
 from .config_utils import ConfigurationException
 from .config_factory import ConfigFactory
+from structlog import configure_once
+from structlog import processors
+from structlog import stdlib
+from structlog import threadlocal
 
-# FIXME
-# This should be moved to the initialization level.
-# See QSP-148.
-# https://quantstamp.atlassian.net/browse/QSP-418
-ConfigUtils().configure_basic_logging()
+
+def configure_basic_logging(verbose=False):
+    logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+    logging.getLogger('botocore').setLevel(logging.CRITICAL)
+
+    configure_once(
+        context_class=threadlocal.wrap_dict(dict),
+        logger_factory=stdlib.LoggerFactory(),
+        wrapper_class=stdlib.BoundLogger,
+        processors=[
+            stdlib.filter_by_level,
+            stdlib.add_logger_name,
+            stdlib.add_log_level,
+            stdlib.PositionalArgumentsFormatter(),
+            processors.TimeStamper(fmt="iso"),
+            processors.StackInfoRenderer(),
+            processors.format_exc_info,
+            processors.UnicodeDecoder(),
+            stdlib.render_to_log_kwargs]
+    )
+
+    dict_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'json': {
+                'format': '%(message)s %(threadName)s %(lineno)d %(pathname)s ',
+                'class': 'pythonjsonlogger.jsonlogger.JsonFormatter'
+            }
+        },
+        'handlers': {
+            'json': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'json'
+            }
+        },
+        'loggers': {
+            '': {
+                'handlers': ['json'],
+                'level': logging.DEBUG if verbose else logging.INFO
+            }
+        }
+    }
+
+    logging.config.dictConfig(dict_config)
+
+
+configure_basic_logging()
