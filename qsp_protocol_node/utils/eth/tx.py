4ed653bd47dac0ecca98dbec559ae5bd1602e673
Martin Derka <martin.derka@seznam.cz>
2018-07-30 10:37:23 -0400

    QSP-500 Implemented mechanism for increasing nonce (#103)
    * Implemented mechanism for increasing nonce
    
    * Added tests for transaction retries
    
    * Fixes QSP-505 and QSP-493

--- qsp_protocol_node/utils/eth/tx.py
+++ qsp_protocol_node/utils/eth/tx.py
@@ -14,12 +14,32 @@ def mk_args(config):
     return args
 
 
-def send_signed_transaction(config, transaction):
+def send_signed_transaction(config, transaction, attempts=10):
     args = mk_args(config)
     if config.account_private_key is None:  # no local signing (in case of tests)
         return transaction.transact(args)
     else:
-        args['nonce'] = config.web3_client.eth.getTransactionCount(config.account)
-        tx = transaction.buildTransaction(args)
-        signed_tx = config.web3_client.eth.account.signTransaction(tx, private_key=config.account_private_key)
-        return config.web3_client.eth.sendRawTransaction(signed_tx.rawTransaction)
+        nonce = config.web3_client.eth.getTransactionCount(config.account)
+        for i in range(attempts):
+            try:
+                args['nonce'] = nonce
+                tx = transaction.buildTransaction(args)
+                signed_tx = config.web3_client.eth.account.signTransaction(tx,
+                                                                           private_key=config.account_private_key)
+                return config.web3_client.eth.sendRawTransaction(signed_tx.rawTransaction)
+            except ValueError as e:
+                if i == attempts - 1:
+                    config.logger.debug("Maximum number of retries reached. {}"
+                                        .format(e))
+                    raise e
+                elif "replacement transaction underpriced" in repr(e):
+                    config.logger.debug("Another transaction is queued with the same nonce. {}"
+                                        .format(e))
+                    nonce += 1
+                elif "known transaction" in repr(e):
+                    # the de-duplication is preserved and the exception is re-raised
+                    config.logger.debug("Transaction deduplication happened. {}".format(e))
+                    raise e
+                else:
+                    config.logger.error("Unknown error while sending transaction. {}".format(e))
+                    raise e
