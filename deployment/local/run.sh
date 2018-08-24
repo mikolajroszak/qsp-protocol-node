90b28ecdc0f113838be3c5f8ee96bd0fb5f5da93
Martin Derka <martin.derka@seznam.cz>
2018-08-24 12:51:03 -0400

    QSP-606 Database persistence (#163)
    * Persisted the database between runs
    
    * Created the file if it does not exist

--- deployment/local/run.sh
+++ deployment/local/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+
+touch $PWD/event_database.db
+
 docker load --input app.tar && \
 docker run -it \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /tmp:/tmp:Z \
 	-v $PWD/keystore:/app/keystore:Z \
 	-v $PWD/config.yaml:/app/config.yaml:Z \
+	-v $PWD/event_database.db:/app/.audit_node.db:Z \
 	-e ENV="testnet" \
 	-e CONFIG="config.yaml" \
 	-e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
