627c913f9beeacf9cf6e346654ae071597a218dd
Leonardo Passos <leo@quantstamp.com>
2019-02-28 11:43:40 -0500

    Adding ReportEncoderFactory

--- src/qsp_protocol_node/audit/factory/report_encoder_factory.py
+++ src/qsp_protocol_node/audit/factory/report_encoder_factory.py
@@ -1,3 +1,10 @@
-class ReportEncoderFactory:
-    pass
-    # TODO
+from component import BaseConfigHandler
+from component import BaseComponentFactory
+
+class ReportEncoderFactory(BaseComponentFactory):
+    def __init__(self, component_name):
+        super().__init__(ReportEncoderConfigHander(component_name))
+
+    def create_component(self, config, context=None):
+        return ReportEncoder(BaseConfigHandler(config))
+    
