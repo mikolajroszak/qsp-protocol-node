b4b12675cc1e119da6b6b0f603aa96666b28c15e
Leonardo Passos <leo@quantstamp.com>
2019-03-01 13:42:23 -0500

    Fixing imports; adding some missing behavior to some TODOs in factories (2)

---
+++ src/qsp_protocol_node/stream/log_stream_provider.py
@@ -0,0 +1,14 @@
+####################################################################################################
+#                                                                                                  #
+# (c) 2018 Quantstamp, Inc. All rights reserved.  This content shall not be used, copied,          #
+# modified, redistributed, or otherwise disseminated except to the extent expressly authorized by  #
+# Quantstamp for credentialed users. This content and its use are governed by the Quantstamp       #
+# Demonstration License Terms at <https://s3.amazonaws.com/qsp-protocol-license/LICENSE.txt>.      #
+#                                                                                                  #
+####################################################################################################
+
+from component import BaseConfigComponent
+
+class LogStreamProvider(BaseConfigComponent):
+    def __init__(self, config):
+        super().__init__(config)
