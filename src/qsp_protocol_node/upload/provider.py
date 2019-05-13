d70402f9e9dd795a72068156dc01c8ec6e42c1e2
Martin Derka <martin@quantstamp.com>
2019-05-13 13:04:30 -0400

    QSP-1149 Licenses MIT (#453)
    * Updated licenses

--- src/qsp_protocol_node/upload/provider.py
+++ src/qsp_protocol_node/upload/provider.py
@@ -1,3 +1,11 @@
+####################################################################################################
+#                                                                                                  #
+# (c) 2019 Quantstamp, Inc. This content and its use are governed by the license terms at          #
+# <https://s3.amazonaws.com/qsp-protocol-license/V2_LICENSE.txt>                                   #
+#                                                                                                  #
+####################################################################################################
+
+
 class UploadProvider:
     def upload_report(self, report_as_string, audit_report_hash=None):
         raise Exception("Unimplemented method")
--- tests/helpers/transact.py
+++ tests/helpers/transact.py
@@ -1,3 +1,10 @@
+####################################################################################################
+#                                                                                                  #
+# (c) 2019 Quantstamp, Inc. This content and its use are governed by the license terms at          #
+# <https://s3.amazonaws.com/qsp-protocol-license/V2_LICENSE.txt>                                   #
+#                                                                                                  #
+####################################################################################################
+
 from utils.eth.singleton_lock import SingletonLock
 
 
