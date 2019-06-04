2527771f598f99eac06834d3f4bdced627f6ebec
alex-murashkin <alexmur90@gmail.com>
2019-06-04 11:07:32 -0400

    Tests made version-agnostic. Version bumped

--- src/qsp_protocol_node/config/config.py
+++ src/qsp_protocol_node/config/config.py
@@ -206,7 +206,7 @@ class Config:
         Builds a Config object from a target environment (e.g., test) and an input YAML
         configuration file.
         """
-        self.__node_version = '2.0.1'
+        self.__node_version = '2.0.2'
         self.__analyzers = []
         self.__analyzers_config = []
         self.__audit_contract_name = None
