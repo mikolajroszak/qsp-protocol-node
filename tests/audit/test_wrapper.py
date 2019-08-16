688ab1eca15e56353c2a599a152fc745491f728c
Sung-Shine Lee <32376541+martinetlee@users.noreply.github.com>
2019-08-16 00:33:07 -0400

    Qsp-1233-upgrade-solc-versions (#544)
    * Updated Docker Images for usolc, mythril, and securify
    * Fixed 0.4.26 related issues

--- tests/audit/test_wrapper.py
+++ tests/audit/test_wrapper.py
@@ -13,7 +13,7 @@ from helpers.qsp_test import QSPTest
 
 class TestWrapper(QSPTest):
     __MYTHRIL_METADATA = {'name': 'mythril',
-                          'version': 'c6ad3380cc251ed8c6f59a3e21aafbfb5ceb41a487dbb38e5dfb6beddd123913',
+                          'version': 'ab192ccc8826b964d0e19a93fe7f5615ef56c0c6c88721f96a1d80c5b02ec135',
                           'vulnerabilities_checked': {'Call data forwarded with delegatecall()': {
                               'type': 'delegate_call_to_untrusted_contract'},
                               'Dependence on predictable environment variable': {
@@ -42,10 +42,10 @@ class TestWrapper(QSPTest):
                               'Unchecked CALL return value': {
                                   'type': 'unchecked_call_return_value'},
                               'Unknown': {'type': 'other'}},
-                          'command': 'docker run --rm -v /tmp/.mythril/25:/shared/ -i qspprotocol/mythril-usolc@sha256:c6ad3380cc251ed8c6f59a3e21aafbfb5ceb41a487dbb38e5dfb6beddd123913  -o json -x /shared/x'}
+                          'command': 'docker run --rm -v /tmp/.mythril/25:/shared/ -i qspprotocol/mythril-usolc@sha256:ab192ccc8826b964d0e19a93fe7f5615ef56c0c6c88721f96a1d80c5b02ec135  -o json -x /shared/x'}
 
     __SECURIFY_METADATA = {'name': 'securify',
-                           'version': 'd1f683b3c561b382a86a1ca7d4d1df1d87986c23ee076345a9bf3679a7d707e3',
+                           'version': 'd367b17b6f1ad898a16cf5d663bc95eaf2cefa5de8779590d31575493f9de799',
                            'vulnerabilities_checked': {
                                'MissingInputValidation': {'type': 'missing_input_validation'},
                                'LockedEther': {'type': 'locked_ether'},
@@ -75,7 +75,7 @@ class TestWrapper(QSPTest):
                                    'type': 'transaction_order_dependency_true_positive'},
                                'SecurifyBug': {'type': 'securify_bug'},
                                'Unknown': {'type': 'other'}},
-                           'command': 'docker run --rm -v /tmp/.securify/37:/shared/ -i qspprotocol/securify-usolc@sha256:d1f683b3c561b382a86a1ca7d4d1df1d87986c23ee076345a9bf3679a7d707e3  -fs /shared/x'}
+                           'command': 'docker run --rm -v /tmp/.securify/37:/shared/ -i qspprotocol/securify-usolc@sha256:d367b17b6f1ad898a16cf5d663bc95eaf2cefa5de8779590d31575493f9de799  -fs /shared/x'}
 
     def test_get_metadata(self):
         """
